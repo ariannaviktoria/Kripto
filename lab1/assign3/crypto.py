40b8728f45392bb44f887731c5790992816c2588
Mate Arianna-Viktoria <ariannaviktoria@yahoo.ro>
2023-01-08 15:12:09 +0200

    lab3

--- lab1/assign3/crypto.py
+++ lab1/assign3/crypto.py
@@ -10,6 +10,9 @@ Replace this with a description of the program.
 """
 import math
 from ntpath import join
+import utils
+import string
+import random
 
 def console():
     
@@ -201,84 +204,260 @@ def decrypt_railfence(chipertext, circumference):
   
     return decrypted_text
 
-# Merkle-Hellman Knapsack Cryptosystem
+def encrypt_file(file_name, key):
+    """ 
+    Encrypts the bytes of a file using a key.\n
+    Uses Vigenere cipher.\n
+    Returns a set of bytes for a new file.
+    """
+    key=key.upper()
+    result = []
+    extension = file_name.split('.')[1]
+    if(extension == []):
+        extension_bytes = bytes([0])
+    else:
+        extension_bytes = bytes([len(extension)])
+        extension_bytes += bytes(extension,'utf-8')        
+
+    f = open(file_name,"rb")
+    k=0
+    file_bytes = f.read()
+    
+    for byte in file_bytes:
+        num = byte
+        if key[k].isalpha():
+            num = num+ string.ascii_uppercase.index(key[k])
+        else:
+            num = num+ string.digits.index(key[k])
+        num = num % 256
+        result.append(num)
+        k=(k+1)%len(key)
+
+    f.close()
+    return extension_bytes + bytes(result)
+
+
+def decrypt_file(file_name, key):
+    """ 
+    Decrypts the bytes of a file using a key. \n
+    Uses Vigenere cipher.\n
+    Returns a set of bytes for the original file.
+    """
+    key=key.upper()
+    result = []
+    f = open(file_name,"rb")
+    extension_length = int.from_bytes(f.read(1), "little")
+    extension = f.read(extension_length).decode("utf-8")
+
+    k=0
+    file_bytes= f.read()
+    for byte in file_bytes:
+        num = byte
+        if key[k].isalpha():
+            num = num - string.ascii_uppercase.index(key[k])
+        else:
+            num = num - string.digits.index(key[k])
+        if num<0:
+            num = 256 + num
+        result.append(num)
+        k=(k+1)%len(key)
+    f.close()
+    return bytes(result),extension
+
+
+#Merkle-Helman Knapsack
 
 def generate_private_key(n=8):
-    """Generate a private key for use in the Merkle-Hellman Knapsack Cryptosystem.
-
-    Following the instructions in the handout, construct the private key components
-    of the MH Cryptosystem. This consistutes 3 tasks:
-
-    1. Build a superincreasing sequence `w` of length n
-        (Note: you can check if a sequence is superincreasing with `utils.is_superincreasing(seq)`)
-    2. Choose some integer `q` greater than the sum of all elements in `w`
-    3. Discover an integer `r` between 2 and q that is coprime to `q` (you can use utils.coprime)
-
-    You'll need to use the random module for this function, which has been imported already
-
-    Somehow, you'll have to return all of these values out of this function! Can we do that in Python?!
-
-    @param n bitsize of message to send (default 8)
-    @type n int
-
-    @return 3-tuple `(w, q, r)`, with `w` a n-tuple, and q and r ints.
     """
-    raise NotImplementedError  # Your implementation here
+    :param n: Bitsize of message to send (defaults to 8)
+    :type n: int
 
-def create_public_key(private_key):
-    """Create a public key corresponding to the given private key.
-
-    To accomplish this, you only need to build and return `beta` as described in the handout.
-
-        beta = (b_1, b_2, ..., b_n) where b_i = r × w_i mod q
-
-    Hint: this can be written in one line using a list comprehension
-
-    @param private_key The private key
-    @type private_key 3-tuple `(w, q, r)`, with `w` a n-tuple, and q and r ints.
-
-    @return n-tuple public key
+    :returns: 3-tuple private key `(w, q, r)`, with `w` a n-tuple, and q and r ints.
     """
-    raise NotImplementedError  # Your implementation here
+    
+    w=[]
+    w.append(random.randint(1,10))
+    for i in range(n-1):
+        k=random.randint(sum(w) + 1, 2 * sum(w))
+        w.append(k)
+    if not(utils.is_superincreasing(w)):
+        print("w sequence is not superincreasing!")
+        return -1
+    q=random.randint(sum(w) + 1, 2 * sum(w))
+    r=random.randint(2, q-1)
+    while not(utils.coprime(r, q)):
+        r=random.randint(2, q-1)
+    w_tuple=tuple(w)
+    return (w_tuple, q, r)
 
+def generate_public_key(private_key):
+    """
+    :param private_key: The private key created by generate_private_key.
+    :type private_key: 3-tuple `(w, q, r)`, with `w` a n-tuple, and q and r ints.
+
+    :returns: n-tuple public key
+    """
+    w=private_key[0]
+    q=private_key[1]
+    r=private_key[2]
+    b=[]
+    for i in range(len(w)):
+        b.append((r*w[i])%q)
+    return tuple(b)
 
 def encrypt_mh(message, public_key):
     """Encrypt an outgoing message using a public key.
 
-    1. Separate the message into chunks the size of the public key (in our case, fixed at 8)
-    2. For each byte, determine the 8 bits (the `a_i`s) using `utils.byte_to_bits`
-    3. Encrypt the 8 message bits by computing
-         c = sum of a_i * b_i for i = 1 to n
-    4. Return a list of the encrypted ciphertexts for each chunk in the message
+    :param message: The message to be encrypted.
+    :type message: bytes
+    :param public_key: The public key of the message's recipient.
+    :type public_key: n-tuple of ints
 
-    Hint: think about using `zip` at some point
-
-    @param message The message to be encrypted
-    @type message bytes
-    @param public_key The public key of the desired recipient
-    @type public_key n-tuple of ints
-
-    @return list of ints representing encrypted bytes
+    :returns: Encrypted message bytes represented as a list of ints.
     """
-    raise NotImplementedError  # Your implementation here
+    c=[]
+    for message_byte in message:
+        bits=utils.byte_to_bits(message_byte)
+        s=0
+        for i in range(len(public_key)):
+            s=s + (bits[i]*public_key[i])
+        c.append(s)
+    return c
 
 def decrypt_mh(message, private_key):
-    """Decrypt an incoming message using a private key
-
-    1. Extract w, q, and r from the private key
-    2. Compute s, the modular inverse of r mod q, using the
-        Extended Euclidean algorithm (implemented at `utils.modinv(r, q)`)
-    3. For each byte-sized chunk, compute
-         c' = cs (mod q)
-    4. Solve the superincreasing subset sum using c' and w to recover the original byte
-    5. Reconsitite the encrypted bytes to get the original message back
-
-    @param message Encrypted message chunks
-    @type message list of ints
-    @param private_key The private key of the recipient
-    @type private_key 3-tuple of w, q, and r
-
-    @return bytearray or str of decrypted characters
     """
-    raise NotImplementedError  # Your implementation here
+    :param message: Encrypted message chunks.
+    :type message: list of ints
+    :param private_key: The private key of the recipient (you).
+    :type private_key: 3-tuple of w, q, and r
 
+    :returns: bytearray of decrypted characters
+    """
+    w=private_key[0]
+    n=len(w)
+    q=private_key[1]
+    r=private_key[2]
+    s=utils.modinv(r, q)
+    decrypted_message=[]
+    for num in message:
+        c=(num*s)%q
+        X=[]
+        for i in range(n):
+            if c>=w[n-i-1]:
+                c-=w[n-i-1]
+                X.append(n-i-1)
+        k=0
+        for x in X:
+            k=k + 2**(n-1-x)
+
+        decrypted_message.append(k)
+            
+    return bytes(decrypted_message)
+
+
+####################
+# SOLITAIRE CIPHER #
+####################
+
+def generate_card_deck(key):
+    card_deck = []
+    for i in range(1,55):
+        card_deck.append(i)
+    
+    for character in key:
+        tripleCut(card_deck)
+        countCut(card_deck)
+        tripleCut(card_deck)
+
+    return card_deck
+
+def moveA(index,card_deck):
+    card_deck[index]=card_deck[(index+1)%54]
+    card_deck[(index+1)%54]=53
+
+def moveB(index,card_deck):
+    card_deck[index]=card_deck[(index+1)%54]
+    card_deck[(index+1)%54]=card_deck[(index+2)%54]
+    card_deck[(index+2)%54]=54
+
+def tripleCut(card_deck):
+    part1=[]
+    part2=[]
+    part3=[]
+
+    i=0
+    while(card_deck[i]!=53 and card_deck[i]!=54):
+        part1.append(card_deck[i])
+        i+=1
+    part2.append(card_deck[i])
+    i+=1
+    while(card_deck[i]!=53 and card_deck[i]!=54):
+        part2.append(card_deck[i])
+        i+=1
+    part2.append(card_deck[i])
+    i+=1
+    while(i<54):
+        part3.append(card_deck[i])
+        i+=1
+    card_deck=part3+part2+part1
+
+def countCut(card_deck):
+    last_card=card_deck[53]
+    pack=[]
+    for i in range(last_card):
+        pack.append(card_deck[i])
+    for i in range(last_card,53):
+        card_deck[i-last_card]=card_deck[i]
+    k=0
+    for pos in range(53-last_card,53):
+        card_deck[pos]=pack[k]
+        k+=1
+
+def generate_keystream_value(card_deck):
+    kv=0
+    #1.Locate jokers and move them
+    jokerA=card_deck.index(53)
+    moveA(jokerA,card_deck)
+    jokerB=card_deck.index(54)
+    moveB(jokerB,card_deck)
+
+
+    #2.Perform triple cut
+    tripleCut(card_deck)
+    #3.Perform count cut
+    countCut(card_deck)
+
+    #4.Return keystream number, if its a joker -> repeat
+    first_card=card_deck[0]
+    kv=card_deck[first_card%54]
+    if (kv==53 or kv==54):
+        return generate_keystream_value(card_deck)
+    else:
+        return kv
+
+def solitaire_fix(text):
+    string_list = list(text.upper())
+    for i in range(len(text)):
+        if not string_list[i] in string.ascii_letters:
+            string_list[i] = 'X'
+    return string_list
+
+def encrypt_solitaire(card_deck,plaintext):
+    ciphertext=""
+    plaintext = solitaire_fix(plaintext)
+    for character in plaintext:
+        plain_number=string.ascii_uppercase.index(character)
+        keystream_value=generate_keystream_value(card_deck)
+        ciphertext+=string.ascii_uppercase[(plain_number+keystream_value)%26]
+    return ciphertext
+
+def decrypt_solitaire(card_deck,ciphertext):
+    plaintext=""
+    for character in ciphertext:
+        cipher_number=string.ascii_uppercase.index(character)
+        keystream_value=generate_keystream_value(card_deck)
+        plain_number=cipher_number-keystream_value
+        while(plain_number<0):
+            plain_number+=26
+        plaintext+=string.ascii_uppercase[plain_number]
+    return plaintext
